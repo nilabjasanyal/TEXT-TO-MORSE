46737d68aec4b603b908f15c787e6e9b1ac8e7b2
nilabjasanyal <nilabja.sanyal@gmail.com>
2021-07-27 13:58:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from code import code
 string = input("INPUT THE STRING TO BE CONVERTED: ")
 
-words = string.split(" ")
-
 converted_code = []
 new_string=""
 for word in string:
@@ -10,7 +8,7 @@ for word in string:
         c_w = code[letter]
         converted_code+=[c_w]
     new_string+="".join(converted_code)
-    # print(converted_code)
+    
 
 print(" ".join(converted_code))
 
